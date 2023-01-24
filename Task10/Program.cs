6f987e0826a293fdde2eb895b3eb0d180ac275e2
Masha <suvorova-2007@mail.ru>
2023-01-24 16:48:52 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int GetNumber()
+{
+    Console.WriteLine("Введите трехзначное число");
+    int threepositionnumber = Convert.ToInt32(Console.ReadLine());
+    return threepositionnumber;
+}
+
+int WriteSecondNumber(int num)
+{
+    int secondfigure = ((num/10)%10);
+    return secondfigure;
+}
+
+//int number = GetNumber();
+//int secondnumber = WriteSecondNumber(number);
+Console.WriteLine($"вторая цифра {WriteSecondNumber(GetNumber())}");
